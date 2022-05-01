777529c2f4de26bc88d26e9f6fe74286958d64a1
Andrei Panferov <andrei@blacksamorez.ru>
2022-05-01 11:35:33 +0000

    Hosted on Yandex Cloud

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram.ext.dispatcher import run_async
 from telegram import Update
 
 import logging
@@ -9,15 +10,21 @@ TEMP = 1.5
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-def toxify(update: Update, context: CallbackContext):
+@run_async
+def toxify_private(update: Update, context: CallbackContext):
+    logger.info("Entering private toxification")
     reply = "Something went wrong!"
     try:
     	reply = requests.post("http://localhost:8080/predict", json={"text": update.message.text, "temp": TEMP}).json()["toxified"]
-    except RuntimeError as er:
+    except ConnectionError as er:
     	logging.exception("No response")
     	pass
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply)
+
+@run_async
+def toxify_group(update: Update, context: CallbackContext):
+    logger.info("Entering group toxification")
 
 def update_temp(update: Update, context: CallbackContext):
 	global TEMP
@@ -25,12 +32,28 @@ def update_temp(update: Update, context: CallbackContext):
 		TEMP = float(context.args[0])
 	context.bot.send_message(chat_id=update.effective_chat.id, text=f"Set temperature {TEMP}")
 
-if __name__ == "__main__":
+def main():
+	logger.info("Bot restarted")
 	updater = Updater(token='', use_context=True)
 	dispatcher = updater.dispatcher
 
-	toxify_handler = MessageHandler(Filters.text & (~Filters.command), toxify)
-	dispatcher.add_handler(toxify_handler)
+	private_toxify_handler = MessageHandler(Filters.text & (~Filters.command), toxify_private)
+	dispatcher.add_handler(private_toxify_handler)
 	update_temp_handler = CommandHandler("temp", update_temp)
-	dispatcher.add_handler(update_temp_handler)
+ # 	 dispatcher.add_handler(update_temp_handler)
+	logger.info("Ready to poll")
 	updater.start_polling()
+	logger.info("Polling")
+
+from daemonize import Daemonize
+
+pid = "/home/blacksamorez/ebanko/bot/bot.pid"
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler("/home/blacksamorez/ebanko/bot/bot.log", "a")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+keep_fds = [fh.stream.fileno()]
+
+daemon = Daemonize(app="ebanko_bot", pid=pid, action=main, keep_fds=keep_fds)
+daemon.start()

9bc1fc27bc557e58d364d56986ded2335e0b0800
Andrei Panferov <andrei@blacksamorez.ru>
2022-05-05 12:54:01 +0000

    All docker

---
+++ app/app/Ebanko.py
@@ -0,0 +1,28 @@
+import torch
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+DEVICE = "cpu"
+
+torch.set_num_threads(4)
+
+class Ebanko:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("sberbank-ai/ruT5-base")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("model").to(DEVICE)
+    
+    def toxify(self, context, temp=1.5):
+        prefix_tokens = self.prepareInput(context).to(DEVICE)
+        
+        suffix_tokens = self.model.generate(prefix_tokens,
+                                            min_length=len(prefix_tokens) + 5,
+                                            do_sample=True,
+                                            top_k=5,
+                                            temperature=temp)[0].cpu()
+            
+        return self.prepareOutput(suffix_tokens)
+    
+    def prepareInput(self, context):
+        return self.tokenizer.encode(context, return_tensors='pt')
+        
+    def prepareOutput(self, tokens):
+        return self.tokenizer.decode(tokens, skip_special_tokens=True)
---
+++ app/app/server.py
@@ -0,0 +1,31 @@
+import json
+
+from Ebanko import Ebanko
+
+from flask import Flask, request, jsonify
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from prometheus_client import Counter
+
+
+app = Flask(__name__, static_url_path="")
+metrics = GunicornInternalPrometheusMetrics(app)
+
+model = Ebanko()
+
+@app.route("/predict", methods=['POST'])
+@metrics.gauge("api_in_progress", "requests in progress")
+@metrics.counter("api_invocations_total", "number of invocations")
+def predict():
+    data = request.get_json(force=True)
+    text = data['text']
+    temp = data['temp']
+
+    result = model.toxify(text, temp)
+
+    return jsonify({
+        "toxified": result
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
